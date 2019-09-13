58a5cb28fefbe8e785c4b1c6660c22dd62316a10
Diogo Franco <web@diogofranco.com.br>
2019-09-12 21:46:47 -0300

    refactored code

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import api from '../services/api';
 import io from 'socket.io-client';
 
@@ -9,34 +9,35 @@ import like from '../assets/like.svg';
 import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
-class Feed extends Component {
+class Feed extends React.Component {
     state = {
-        feed: [],
+        feed: []
     };
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        })
+
+       socket.on('like', likedPost => {
+           this.setState({
+               feed: this.state.feed.map(post => 
+                   post._id === likedPost._id ? likedPost : post
+               )
+           });
+       })
+   }
 
     async componentDidMount() {
         this.registerToSocket();
-
         const response = await api.get('posts');
 
         this.setState({ feed: response.data});
     }
 
-    registerToSocket = () => {
-         const socket = io('http://localhost:3333');
-
-         socket.on('post', newPost => {
-             this.setState({ feed: [newPost, ...this.state.feed] });
-         })
-
-        socket.on('like', likedPost => {
-            this.setState({
-                feed: this.state.feed.map(post => 
-                    post._id === likedPost._id ? likedPost : post
-                )
-            });
-        })
-    }
+    
 
     handleLike = id => {
         api.post(`/posts/${id}/like`);
